2d179ceb3ec6f8f03b78e4f160c10a07b47e7221
Robert Lovelock <roblovelock@gmail.com>
2026-03-26 11:22:09 +0000

    fix: remember your cpp syntax!

--- components/aliexpress_ultrasonic/aliexpress_ultrasonic_sensor.cpp
+++ components/aliexpress_ultrasonic/aliexpress_ultrasonic_sensor.cpp
@@ -58,7 +58,7 @@ void UltrasonicSensorComponent::loop() {
 
   if (!this->store_.echo_start) {
     uint32_t elapsed = micros() - this->measurement_start_us_;
-    if (elapsed >= this.start_timeout_us_) {
+    if (elapsed >= this->start_timeout_us_) {
       ESP_LOGW(TAG, "'%s' - Measurement start timed out", this->name_.c_str());
       this->publish_state(NAN);
       this->measurement_pending_ = false;
